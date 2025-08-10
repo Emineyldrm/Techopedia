370db6ae0d343bb2a717753b519bad1bb0ab2871
Emine Yıldırım <emineyildirim2526@gmail.com>
2025-08-10 21:49:19 +0300

    Add fragment layouts and UI components

---
+++ app/src/main/java/com/emine/techopedia/TechDetailFragment.kt
@@ -0,0 +1,44 @@
+package com.emine.techopedia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.emine.techopedia.databinding.FragmentTechDetailBinding
+
+
+class TechDetailFragment : Fragment() {
+
+    private var _binding: FragmentTechDetailBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTechDetailBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
+}
